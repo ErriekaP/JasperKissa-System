dfeadcb662e75c6cae1d1283a195ba44aa7a9cf5
Mdltre <68366944+Mdltre@users.noreply.github.com>
2022-10-11 14:02:18 +0800

    AAAAAAAAA

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -88,3 +88,80 @@ if (sess==true) {
   res.render("errorlogin", {title: 'Error!', layout: 'empty'});
 }
 };
+
+exports.emprofile = (req,res) => {
+  var sess= req.app.locals.sess;
+  var empusername = req.app.locals.empname;
+
+if (sess==true) {
+    //Connect to DB
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      connection.query('SELECT * FROM employee WHERE username= ?', [empusername], (err, rows)=> {
+        connection.release();
+  
+        if(!err){
+          res.render('my_employeeprofile', {rows, layout: 'empty'});
+        } else {
+          console.log(err);
+        }
+      });
+    });
+} else {
+  // Not logged in
+  res.render("errorlogin", {title: 'Error!', layout: 'empty'});
+}
+};
+
+exports.other_emprofile = (req,res) => {
+  var sess= req.app.locals.sess;
+  var empid = req.params.id;
+
+if (sess==true) {
+    //Connect to DB
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      connection.query('SELECT * FROM employee WHERE emp_id= ?', [empid], (err, rows)=> {
+        connection.release();
+  
+        if(!err){
+          res.render('employeeprofile', {rows, layout: 'empty'});
+        } else {
+          console.log(err);
+        }
+      });
+    });
+} else {
+  // Not logged in
+  res.render("errorlogin", {title: 'Error!', layout: 'empty'});
+}
+};
+
+exports.empattrecord = (req,res) => {
+  var sess= req.app.locals.sess;
+  var empusername = req.app.locals.empname;
+
+if (sess==true) {
+    //Connect to DB
+    pool.getConnection((err, connection) => {
+      const type = "";
+  
+      connection.query('SELECT attendanceID, employeeID, attendance_type,attendance_name, DATE_FORMAT(attendance_dt,"%M %d, %Y") as datein,TIME_FORMAT(attendance_dt, "%I:%i:%s %p") as timein,DAYNAME(attendance_dt) as DN FROM attendance_records,employee WHERE attendance_records.employeeID = employee.emp_id AND employee.username = ?', [empusername], (err, rows)=> {
+        connection.release();
+  
+        if(!err){
+          
+          res.render("employee_ownattrecord", {rows, layout: 'empty'});
+         
+        } else {
+          console.log(err);
+        }
+      
+        
+       });
+    });
+  } else {
+  // Not logged in
+  res.render("errorlogin", {title: 'Error!', layout: 'empty'});
+}
+};
--- server/routes/employee.js
+++ server/routes/employee.js
@@ -5,6 +5,16 @@ const employeeController = require('../controllers/employeeController');
 //main employee page
 router.get('/employee/home', employeeController.mainHome);
 
+//Go to employee profile
+router.get('/myemployeeprofile', employeeController.emprofile);
+
+//look at other people's employee profile
+router.get('/employeeprofile/:id', employeeController.other_emprofile);
+
+//look at own attendance records
+router.get('/attendancerecord', employeeController.empattrecord);
+
+
 //view and find employee
 router.get('/employeelist', employeeController.view);
 router.post('/employeelist', employeeController.find);
